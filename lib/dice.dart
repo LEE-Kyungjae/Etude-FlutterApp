f1be0c2673937bbcba3c2fb050ce1dbc6a09f2d7
LEE-Kyungjae <levelchecking@gmail.com>
2022-07-26 16:11:38 +0900

    version 0.0.1

--- lib/dice.dart
+++ lib/dice.dart
@@ -31,7 +31,7 @@ class _DiceState extends State<Dice> {
   TextEditingController controller2 = TextEditingController();
   TextEditingController controller3 = TextEditingController();
   TextEditingController controller4 = TextEditingController();
-
+//a
   @override
   Widget build(BuildContext context) {
     return Scaffold(
