eeebcb63144b35900d2891721be58990a8a33855
LEE-Kyungjae <levelchecking@gmail.com>
2022-10-19 01:27:13 +0900

    new

--- lib/AdMob/ad_mob_service.dart
+++ lib/AdMob/ad_mob_service.dart
@@ -18,29 +18,20 @@
 // nativeAdvanced	ca-app-pub-3940256099942544/3986624511
 // nativeAdvancedVideo	ca-app-pub-3940256099942544/2521693316
 
-
 import 'dart:io' show Platform;
 import 'package:flutter/cupertino.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 class AdMobService {
-  // static String? get  {
+  //앱시작
+  // static String? get appOpenAdUnitId {
   //   if (Platform.isAndroid) {
-  //     return '';
+  //     return 'ca-app-pub-3940256099942544/3419835294';
   //   } else if (Platform.isIOS) {
-  //     return '';
+  //     return 'ca-app-pub-3940256099942544/5662855259';
   //   }
   //   return null;
-  //}
-  //앱시작
-  static String? get appOpenAdUnitId {
-    if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/3419835294';
-    } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/5662855259';
-    }
-    return null;
-  }
+  // }
   //배너
   static String? get bannerAdUnitId {
     if (Platform.isAndroid) {
@@ -51,38 +42,38 @@ class AdMobService {
     return null;
   }
 //전면
-  static String? get interstitialAdUnitId {
-    if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/1033173712';
-    } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/4411468910';
-    }
-    return null;
-  }
-  static String? get interstitialVideoAdUnitId {
-    if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/8691691433';
-    } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/5135589807';
-    }
-    return null;
-  }
-  static String? get rewardedAdUnitId {
-    if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/5224354917';
-    } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/1712485313';
-    }
-    return null;
-  }
-  static String? get rewardedInterstitialAdUnitId {
-    if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/5354046379';
-    } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/6978759866';
-    }
-    return null;
-  }
+//   static String? get interstitialAdUnitId {
+//     if (Platform.isAndroid) {
+//       return 'ca-app-pub-3940256099942544/1033173712';
+//     } else if (Platform.isIOS) {
+//       return 'ca-app-pub-3940256099942544/4411468910';
+//     }
+//     return null;
+//   }
+//   static String? get interstitialVideoAdUnitId {
+//     if (Platform.isAndroid) {
+//       return 'ca-app-pub-3940256099942544/8691691433';
+//     } else if (Platform.isIOS) {
+//       return 'ca-app-pub-3940256099942544/5135589807';
+//     }
+//     return null;
+//   }
+//   static String? get rewardedAdUnitId {
+//     if (Platform.isAndroid) {
+//       return 'ca-app-pub-3940256099942544/5224354917';
+//     } else if (Platform.isIOS) {
+//       return 'ca-app-pub-3940256099942544/1712485313';
+//     }
+//     return null;
+//   }
+//   static String? get rewardedInterstitialAdUnitId {
+//     if (Platform.isAndroid) {
+//       return 'ca-app-pub-3940256099942544/5354046379';
+//     } else if (Platform.isIOS) {
+//       return 'ca-app-pub-3940256099942544/6978759866';
+//     }
+//     return null;
+//   }
 
 
   static final BannerAdListener bannerListener = BannerAdListener(
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
-//import 'package:mafiaexp/LoginD/logind.dart';
 import 'package:mafiaexp/MainD/home_page.dart';
 import 'package:mafiaexp/provider/google_sign_in.dart';
 import 'package:provider/provider.dart';
-//import 'package:mafiaexp/MainD/CalcD/CardD/Card.dart';
 
 Future main() async{
   //스플래시 스크린때 데이터받기
@@ -25,15 +23,13 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'shadowgovernment',
         home: HomePage(),
-
       ),
     );
   }
   Future<bool> fetchData()async{
   bool data=false;
-  await Future.delayed(const Duration(seconds: 3),(){
+  await Future.delayed(const Duration(seconds: 1),(){
     data = true;
   });
   return data;
   }
-
