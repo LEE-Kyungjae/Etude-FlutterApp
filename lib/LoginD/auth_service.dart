aaa4a8ed08619ef0fc048f05540e7984c3fb2996
LEE-Kyungjae <levelchecking@gmail.com>
2022-10-19 02:44:54 +0900

    new

---
+++ lib/LoginD/auth_service.dart
@@ -0,0 +1,15 @@
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+// class AuthService(){
+//   //handle Authstate
+//   //signinWithGoogle()
+// //signOut()
+//
+
+
+
+
+
+//}
